0f85be50231989530acd8584fc0d7afe2db9fe14
unknown <valrus1808@gmail.com>
2026-03-06 00:15:01 +0200

    update: file test updated

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -6,25 +6,30 @@
 // tree, read text, and verify that the values of widget properties are correct.
 
 import 'package:flutter/material.dart';
-import 'package:flutter_test/flutter_test.dart';
 
 import 'package:flutter_colours_randomizer/flutter_colour_randomizer.dart';
+import 'package:flutter_colours_randomizer/providers/theme_provider.dart';
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const FlutterColourRandomizer());
+    SharedPreferences.setMockInitialValues({});
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    await tester.pumpWidget(
+      ChangeNotifierProvider(
+        create: (_) => ThemeProvider(),
+        child: const MaterialApp(
+          home: FlutterColourRandomizer(),
+        ),
+      ),
+    );
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
+    expect(find.text('Hello there'), findsOneWidget);
+
+    await tester.tap(find.byType(GestureDetector));
     await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
   });
 }
